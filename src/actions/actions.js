e616e5cda936019f35dc2a30a1500615f684304c
JimvandeVen <Jim.van.de.Ven@hva.nl>
2020-02-19 12:26:42 +0100

    it works a bit...

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,23 +1,6 @@
-export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
-
-export function selectSubreddit(subreddit) {
-  return {
-    type: SELECT_SUBREDDIT,
-    subreddit,
-  }
-}
-
-export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
-
-export function invalidateSubreddit(subreddit) {
-  return {
-    type: INVALIDATE_SUBREDDIT,
-    subreddit,
-  }
-}
+import fetch from 'cross-fetch'
 
 export const REQUEST_POSTS = 'REQUEST_POSTS'
-
 function requestPosts(subreddit) {
   return {
     type: REQUEST_POSTS,
@@ -26,12 +9,58 @@ function requestPosts(subreddit) {
 }
 
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-
 function receivePosts(subreddit, json) {
   return {
     type: RECEIVE_POSTS,
     subreddit,
     posts: json.data.children.map((child) => child.data),
     receivedAt: Date.now(),
+  }
+}
+
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
+export function invalidateSubreddit(subreddit) {
+  return {
+    type: INVALIDATE_SUBREDDIT,
+    subreddit,
+  }
+}
+
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
+export function selectSubreddit(subreddit) {
+  return {
+    type: SELECT_SUBREDDIT,
+    subreddit,
+  }
+}
+
+export function fetchPosts(subreddit) {
+  return (dispatch) => {
+    dispatch(requestPosts(subreddit))
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then(
+        (response) => response.json(),
+        (error) => console.log('An error occured', error),
+      )
+      .then((json) => dispatch(receivePosts(subreddit, json)))
+  }
+}
+
+function shouldFetchPosts(state, subreddit) {
+  const posts = state.postsBySubreddit[subreddit]
+  if (!posts) {
+    return true
+  } else if (posts.isFetching) {
+    return false
+  }
+  return posts.didInvalidate
+}
+
+export function fetchPostsIfNeeded(subreddit) {
+  return (dispatch, getState) => {
+    if (shouldFetchPosts(getState(), subreddit)) {
+      return dispatch(fetchPosts(subreddit))
+    }
+    return Promise.resolve()
   }
 }
